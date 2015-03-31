6adf7ee9a052addd8d79e2347a52d1b7fc2b1293
Anders Janmyr <anders@janmyr.com>
2015-03-31 15:04:38 +0200

    Fixed test desc

--- node/test/NAME.da-test.js
+++ node/test/NAME.da-test.js
@@ -4,7 +4,7 @@ var assert = require("assert")
 var {{dromedarcase .NAME}} = require("../lib/{{dasherized .NAME}}");
 
 describe('{{dromedarcase .NAME}}', function(){
-  it('should return -1 when the value is not present', function(){
+  it('returns the name string', function(){
     var actual = {{dromedarcase .NAME}}();
     assert.equal(actual, "{{dromedarcase .NAME}}");
   });

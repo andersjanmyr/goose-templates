3fb65ed908d0d82d73fa09cb7e5c54f248350c2a
Anders Janmyr <anders@janmyr.com>
2016-02-02 09:51:31 +0100

    Wrapper function to allow initiating the model.

--- seq-model/lib/models/NAME.da.js
+++ seq-model/lib/models/NAME.da.js
@@ -1,13 +1,12 @@
 'use strict';
-const sequelize = require('sequelize');
-
-const {{camelcase .NAME}} = sequelize.define('{{snakecase .NAME}}s', {
-  id: { type: Sequelize.UUID, primaryKey: true },
-  {{ range $key, $value := .DATA}}
-  {{dromedarcase $key}}: { field: '{{snakecase $key}}' type: Sequelize.{{boacase $value}} },
-  {{ end }}
-  createdAt: { field: 'created_at', type: Sequelize.DATE },
-  updatedAt: { field: 'updated_at', type: Sequelize.DATE }
-});
-
-module.exports = {{camelcase .NAME}};
+module.exports = function(sequelize, DataTypes) {
+  const {{camelcase .NAME}} = sequelize.define('{{snakecase .NAME}}s', {
+    id: { type: DataTypes.UUID, primaryKey: true },
+    {{ range $key, $value := .DATA}}
+    {{dromedarcase $key}}: { field: '{{snakecase $key}}', type: DataTypes.{{boacase $value}} },
+    {{ end }}
+    createdAt: { field: 'created_at', type: DataTypes.DATE },
+    updatedAt: { field: 'updated_at', type: DataTypes.DATE }
+  });
+  return {{camelcase .NAME}};
+};
